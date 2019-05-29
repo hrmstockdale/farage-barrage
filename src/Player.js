e261854858ddca4f6788344cf44434e69d70bc7e
Bobby <bobbysebolao@gmail.com>
2019-05-29 23:07:21 +0100

    Randomize player image position on click via randomCoords.js

--- src/Player.js
+++ src/Player.js
@@ -1,39 +1,44 @@
 import React from "react";
 import { getUserData } from "./utils/data_helpers";
-
+import {
+  getViewportSize,
+  randomCoords,
+  getRandomInt
+} from "./utils/randomCoords";
 
 const Player = props => {
   const [img, setImg] = React.useState(null);
   const [position, setPosition] = React.useState({
-    position: 'absolute',
+    position: "absolute",
     top: 50,
     left: 50
   });
 
   React.useEffect(() => {
-    const username = 'bobbysebolao';
-    getUserData(username)
-    .then(response  => setImg(response.avatar_url))
+    const username = "bobbysebolao";
+    getUserData(username).then(response => setImg(response.avatar_url));
   }, []);
-  
-  const randomize = () => setPosition({
-    top:100,
-    left: 100
-  })
+
+  const randomize = () => setPosition(randomCoords(getViewportSize()));
 
   const divStyle = {
-    position: 'relative',
-    background: 'red',
-    border: '1px solid black',
-    height: '100px',
-    width: '100px'
-  }
+    position: "relative",
+    background: "red",
+    border: "1px solid black",
+    height: "100px",
+    width: "100px"
+  };
 
-  return(
-    <div style = {divStyle}>
-      <img src={img} style={position} alt="from your github" onClick={randomize}/>
+  return (
+    <div style={divStyle}>
+      <img
+        src={img}
+        style={position}
+        alt="from your github"
+        onClick={randomize}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Player;
--- src/token.js
+++ src/token.js
@@ -1,3 +1,3 @@
-const token = "d56bd2d5f23b8fb6019cc56d52f72e4af3eecfc9";
+const token = "5b1915aedfb7f4af1dac3bf6437bb0fe12d5c510";
 
 export default token;
