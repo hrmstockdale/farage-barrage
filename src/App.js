69447cd2cb769dc0a41509f5395381bccd7ba167
Bobby <bobbysebolao@gmail.com>
2019-05-30 17:38:20 +0100

    Add Farage component and functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Player from "./Player/Player";
 import Scoreboard from "./Scoreboard/Scoreboard";
+import Farage from "./Farage/Farage.js";
 
 function App() {
   const [points, setPoints] = React.useState(0);
@@ -9,10 +10,15 @@ function App() {
     setPoints(prevPoints => prevPoints + 1);
   };
 
+  const minusPlayerOnClick = prevPoints => {
+    setPoints(prevPoints => prevPoints - 1);
+  };
+
   return (
     <div className="App">
       <Scoreboard data={points} />
-      <Player updateScore={plusPlayerOnClick} />
+      <Player updateScore={minusPlayerOnClick} />
+      <Farage updateScore={plusPlayerOnClick} />
       <header className="App-header" />
     </div>
   );
